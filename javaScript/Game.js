bd22e8359bd6eb344902d1c788df96aa05cb0d24
Michał Biernacki <michalbiernacki95@o2.pl>
2022-02-02 09:14:28 +0100

    commit

--- javaScript/Game.js
+++ javaScript/Game.js
@@ -97,7 +97,7 @@ class Game {
     }
 
     createDirectionalLight() {
-        const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+        const directionalLight = new THREE.DirectionalLight(0xffffff, 0.1);
         directionalLight.position.set(0, 1, 0);
         directionalLight.castShadow = false;
         this.scene.add(directionalLight);
